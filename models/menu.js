655442fa05c3d28f2ce3fc022158b9d4525f2967
timzprof <timilehin.olumofin@gmail.com>
2019-02-08 02:49:32 +0100

    Added Models

---
+++ models/menu.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'menu.json');
+
+// const getMealsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
+
+class Menu {
+  constructor(caterer, meals) {
+    this.caterer = caterer;
+    this.meals = meals;
+  }
+}
+
+export default Menu;
