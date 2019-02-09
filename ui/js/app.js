03108513f14884fe43574d255d2f61cd76109e4b
timzprof <timilehin.olumofin@gmail.com>
2019-02-09 20:07:31 +0100

    Added gitignore file and Changes Cart page to Orders Page

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,123 +1,122 @@
-let overlay = document.querySelector('.overlay');
-let mobileMenu = document.querySelector('.mobile-menu');
+let overlay = document.querySelector(".overlay");
+let mobileMenu = document.querySelector(".mobile-menu");
 
-let redirect = (location) => {
-  window.location.href = location + '.html';
-}
+let redirect = location => {
+  window.location.href = location + ".html";
+};
 
 let toggleMobileNav = () => {
-  let mobileNav = document.querySelector('.mobile-nav');
-  if (mobileNav.classList.contains('hide')) {
-    overlay.classList.remove('hide');
-    mobileMenu.innerHTML = '&#9747;';
+  let mobileNav = document.querySelector(".mobile-nav");
+  if (mobileNav.classList.contains("hide")) {
+    overlay.classList.remove("hide");
+    mobileMenu.innerHTML = "&#9747;";
   } else {
-    overlay.classList.add('hide');
-    mobileMenu.innerHTML = '&#9776;';
+    overlay.classList.add("hide");
+    mobileMenu.innerHTML = "&#9776;";
   }
-  mobileNav.classList.toggle('hide');
-}
+  mobileNav.classList.toggle("hide");
+};
 
 iziToast.settings({
   timeout: 1500,
-  resetOnHover: true,
+  resetOnHover: true
 });
 
-overlay.addEventListener('click', toggleMobileNav);
+overlay.addEventListener("click", toggleMobileNav);
 
-mobileMenu.addEventListener('click', toggleMobileNav);
+mobileMenu.addEventListener("click", toggleMobileNav);
 
+let orderBtns = document.querySelectorAll(".order-btn");
 
-let orderBtns = document.querySelectorAll('.order-btn');
-
-if(orderBtns !== null) {
+if (orderBtns !== null) {
   orderBtns.forEach(orderBtn => {
-    orderBtn.addEventListener('click', () => {
-      overlay.classList.remove('hide');
+    orderBtn.addEventListener("click", () => {
+      overlay.classList.remove("hide");
       iziToast.show({
-        color: 'green',
-        icon: 'ico-success',
-        title: 'Success',
-        message: 'Adding to Cart..',
-        position: 'center', 
+        color: "green",
+        icon: "ico-success",
+        title: "Success",
+        message: "Adding to Cart..",
+        position: "center",
         onClosing: () => {
-          redirect('cart');
+          redirect("orders");
         }
       });
     });
   });
 }
 
-let deleteBtns = document.querySelectorAll('.delete-btn');
+let deleteBtns = document.querySelectorAll(".delete-btn");
 
 if (deleteBtns !== null) {
   deleteBtns.forEach(deleteBtn => {
-    deleteBtn.addEventListener('click', () => {
-      overlay.classList.remove('hide');
+    deleteBtn.addEventListener("click", () => {
+      overlay.classList.remove("hide");
       iziToast.show({
-        color: 'green',
-        icon: 'ico-success',
-        title: 'Success',
-        message: 'Item Deleted..',
-        position: 'center',
+        color: "green",
+        icon: "ico-success",
+        title: "Success",
+        message: "Item Deleted..",
+        position: "center",
         onClosing: () => {
-          redirect('cart');
+          redirect("orders");
         }
       });
     });
   });
 }
 
-let loginForm = document.getElementById('loginForm');
+let loginForm = document.getElementById("loginForm");
 
-if(loginForm !== null) {
-  loginForm.addEventListener('submit', (e) => {
+if (loginForm !== null) {
+  loginForm.addEventListener("submit", e => {
     e.preventDefault();
-    overlay.classList.remove('hide');
+    overlay.classList.remove("hide");
     iziToast.show({
-      color: 'green',
-      icon: 'ico-success',
-      title: 'Success',
-      message: 'Logging In..',
-      position: 'center',
+      color: "green",
+      icon: "ico-success",
+      title: "Success",
+      message: "Logging In..",
+      position: "center",
       onClosing: () => {
-        redirect('index');
+        redirect("index");
       }
     });
   });
 }
 
-let registerForm = document.getElementById('registerForm');
+let registerForm = document.getElementById("registerForm");
 
 if (registerForm !== null) {
-  registerForm.addEventListener('submit', (e) => {
+  registerForm.addEventListener("submit", e => {
     e.preventDefault();
-    overlay.classList.remove('hide');
+    overlay.classList.remove("hide");
     iziToast.show({
-      color: 'green',
-      icon: 'ico-success',
-      title: 'Success',
-      message: 'Registration Done..',
-      position: 'center',
+      color: "green",
+      icon: "ico-success",
+      title: "Success",
+      message: "Registration Done..",
+      position: "center",
       onClosing: () => {
-        redirect('index');
+        redirect("index");
       }
     });
   });
 }
 
-let makeOrderBtn = document.getElementById('makeOrder');
+let makeOrderBtn = document.getElementById("makeOrder");
 
 if (makeOrderBtn !== null) {
-  makeOrderBtn.addEventListener('click', () => {
-    overlay.classList.remove('hide');
+  makeOrderBtn.addEventListener("click", () => {
+    overlay.classList.remove("hide");
     iziToast.show({
-      color: 'green',
-      icon: 'ico-success',
-      title: 'Success',
-      message: 'Order Made',
-      position: 'center',
+      color: "green",
+      icon: "ico-success",
+      title: "Success",
+      message: "Order Made",
+      position: "center",
       onClosing: () => {
-        redirect('index');
+        redirect("index");
       }
     });
   });
