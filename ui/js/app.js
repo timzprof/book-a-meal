3ead43f09ff23ba57764b9d14090de669861b87d
timzprof <timilehin.olumofin@gmail.com>
2019-02-10 09:01:51 +0100

    Changed Mobile Toggle to Animated Burger and fixed form page overflow

--- ui/js/app.js
+++ ui/js/app.js
@@ -9,10 +9,10 @@ const toggleMobileNav = () => {
   const mobileNav = document.querySelector('.mobile-nav');
   if (mobileNav.classList.contains('hide')) {
     overlay.classList.remove('hide');
-    mobileMenu.innerHTML = '&#9747;';
+    mobileMenu.classList.add('open');
   } else {
-    overlay.classList.add("hide");
-    mobileMenu.innerHTML = "&#9776;";
+    overlay.classList.add('hide');
+    mobileMenu.classList.remove('open');
   }
   mobileNav.classList.toggle('hide');
 };
