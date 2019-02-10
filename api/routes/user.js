05b700d625de0287a43c693bd05ecf3c1d4d0ba2
timzprof <timilehin.olumofin@gmail.com>
2019-02-10 02:03:51 +0100

    Implemented Get User Orders Endpoint and Refactored Add Order Endpoint

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get('/menu/', userController.getMenus);
 
+router.get('/orders', userController.getOrders);
+
 router.post('/orders', userController.orderMeal);
 
 module.exports = router;
