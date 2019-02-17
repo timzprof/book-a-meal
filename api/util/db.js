0e1e571b1a52d6d241e7f671680847876a8a7ce7
timzprof <timilehin.olumofin@gmail.com>
2019-02-17 16:27:00 +0100

    Modified Database env variables

--- api/util/db.js
+++ api/util/db.js
@@ -3,12 +3,7 @@ import { config } from 'dotenv';
 
 config();
 
-const { DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } = process.env;
-
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  dialect: 'postgres',
-  port: DB_PORT,
-  host: DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 });
 
