9bccfb3090a1c030a1445c39ea51f1f520c992f8
timzprof <timilehin.olumofin@gmail.com>
2019-02-23 00:44:45 +0100

    Updated Build files

--- build/index.js
+++ build/index.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-require("@babel/polyfill");
-
 var _express = _interopRequireDefault(require("express"));
 
 var _expressFileupload = _interopRequireDefault(require("express-fileupload"));
