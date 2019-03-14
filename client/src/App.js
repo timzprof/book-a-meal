993b50b51a2bfd930ee231da375457146b9716c5
timzprof <timilehin.olumofin@gmail.com>
2019-03-14 09:09:27 +0100

    Added Aux Component and Basic Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>Welcome To Book A Meal</h1>
       </div>
     );
