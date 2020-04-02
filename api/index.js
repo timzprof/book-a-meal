1ce778cc478e171bfb89414d25c3c472348ee8d7
timzprof <timilehin.olumofin@gmail.com>
2020-04-03 00:14:18 +0100

    Changed API version number

--- api/index.js
+++ api/index.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/api/v1', Routes);
+app.use('/api/v2', Routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('client/build', 'index.html'));
