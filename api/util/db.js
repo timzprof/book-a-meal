a0719938789d93a51a827e3fb87247c4565128df
timzprof <timilehin.olumofin@gmail.com>
2022-09-19 11:02:46 +0100

    Sequelize Config Changes

--- api/util/db.js
+++ api/util/db.js
@@ -7,7 +7,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: console.log,
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 });
 
