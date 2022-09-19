36fe33b3a57b767cc2f27b50096fd071807d8d73
timzprof <timilehin.olumofin@gmail.com>
2022-09-19 10:56:28 +0100

    Sequelize Config Changes

--- api/util/db.js
+++ api/util/db.js
@@ -4,7 +4,11 @@ import { config } from 'dotenv';
 config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  logging: console.log
+  logging: console.log,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 export default sequelize;
